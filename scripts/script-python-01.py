179557ff5500cf2ed650075f77e462de8e9130e5
maria.sharkina <maria.sharkina@gmail.com>
2021-01-07 17:04:35 -0800

    Fix script interuption.

--- scripts/script-python-01.py
+++ scripts/script-python-01.py
@@ -8,4 +8,3 @@ for result in result_os.split('\n'):
     if result.find('modified') != -1:
         prepare_result = result.replace('\tmodified:   ', '')
         print(os.getcwd() + '/' + prepare_result)
-        break
